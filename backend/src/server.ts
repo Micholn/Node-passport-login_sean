f478f8659fe584935108033558efc7d5083982aa
Micholn <mdiagbare@gmail.com>
2024-03-22 23:32:44 +0100

    swaggerUI

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -13,7 +13,7 @@ import categoryRouter from './Routers/categoryRouter';
 import roleRouter from './Routers/roleRouter';
 import middlewares from './'
 
-import allApis from './'
+import allApis from './swa'
 
 
 
