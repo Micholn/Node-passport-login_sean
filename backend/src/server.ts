dd4b8710868910ca5839bf92439733968b1f62f8
Micholn <mdiagbare@gmail.com>
2024-03-23 23:59:22 +0100

    did the extraction

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -36,7 +36,7 @@ let strategy = new jwtStrategy(jwtOptions, (jwtPayload, next) => {
 
 
 
-Passport.use( express: comfig: s) 
+Passport.use( express: comfig: ss) 
 
 /*load up our environmental variables for development, so we're requiring that development
 dependency */
