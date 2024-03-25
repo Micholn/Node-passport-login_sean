64d01af31a9dbecc128cd42a751f34db39a6ae1f
Micholn <mdiagbare@gmail.com>
2024-03-25 23:51:39 +0100

    made a fix for the lint strategy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -34,7 +34,6 @@ let strategy = new jwtStrategy(jwtOptions, (jwtPayload, next) => {
   next(null, jwtPayload.i
 
 
-
 Passport.use( express: comfig: ss: any: : ) 
 
 /*load up our environmental variables for development, so we're requiring that development
