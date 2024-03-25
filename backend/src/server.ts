5d8345140d8182ba172e050b5561665e77124354
Micholn <mdiagbare@gmail.com>
2024-03-25 23:58:40 +0100

    made a fix for the lint strategy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -30,7 +30,7 @@ let jwtOptions = {
   secretOrK
 
 //eslint-disable-next-line new-cap
-let strategy = new jwtStrategy(jwtOptions, (jwtPayload,
+let strategy = new jwtStrategy(jwtOptions, (jwtPayload
 
 
 Passport.use( express: comfig: ss: any: : ) 
