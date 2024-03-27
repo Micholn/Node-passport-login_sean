b8e52e7f4ae9c371e5e4540836ec1a77a14029b5
Micholn <mdiagbare@gmail.com>
2024-03-27 23:50:55 +0100

    added the controllers for the server admin

--- backend/src/Controllers/adminController.ts
+++ backend/src/Controllers/adminController.ts
@@ -5,7 +5,7 @@ import { mailSend } from "../Services/mailServices";
 import { errorRes, saveRes, successRes } from "../Services/responseServices";
 
 let db = new DbServices();
-let response: an
+let response: a
 }
 
 export class AdminController {
