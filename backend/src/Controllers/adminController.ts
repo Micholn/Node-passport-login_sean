b75037c1767f1c70950663f3b8f9aca4ab7f04d4
Micholn <mdiagbare@gmail.com>
2024-03-27 23:45:03 +0100

    added the controllers for the server admin

--- backend/src/Controllers/adminController.ts
+++ backend/src/Controllers/adminController.ts
@@ -6,7 +6,7 @@ import { errorRes, saveRes, successRes } from "../Services/responseServices";
 
 let db = new DbServices();
 let response: any = {
-    message: "Data fetched successfully"
+    message: "Data fetched successfull
 }
 
 export class AdminController {
